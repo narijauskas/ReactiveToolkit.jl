5b26dc3a047bc96cd8203c950497e0af05c12aa3
mantas <man61@case.edu>
2021-12-31 20:59:35 -0700

    api

--- dev/api.jl
+++ dev/api.jl
@@ -1,4 +1,36 @@
+#= Overview
 
+A framework for reactive programming which makes use of modern features of julia, namely atomic fields and task-based concurrency.
+
+Based on:
+    * Elm
+    * Functional Reactive Programming (and it's derivatives)
+    * Reactive.jl
+    * Observables.jl
+    * practical experience
+
+
+Primary functionality is achieved by creating a directed graph. Nodes are functions/tasks which may run asynchronously (and accross multiple threads). Edges are Signals - a way of sharing data safely.
+
+
+Issues:
+    - how to handle MIMO tasks?
+        - option 1. run task on any signal update
+        - option 2. run task once all signals have updated
+    - signals of signals
+        - just don't allow these to be constructed
+    - synchronization
+        - is it necessary?
+        - is there a way to do it beyond just a task?
+    - stopping timed tasks
+        - option 1. kill tasks
+            task = every(.) 
+            kill!(task)
+        - option 2. use a timer object
+            t = Timer()
+            every(t, .)
+            kill!(t)
+=#
 #------------------------------------ signals ------------------------------------#
 
 x = Signal(1)
@@ -9,8 +41,17 @@ x[] = 1.0
 
 y = x[]
 
+# these should throw an error:
+z = Signal(x)
+z = Signal(Signal(1))
+
 
 #------------------------------------ tasks ------------------------------------#
+# what to return?
+# - actual tasks
+# - task wrappers (eg. ReactiveTask/RepeatingTask)
+# - other signals? (return type of task?)
+# - nothing
 
 on(s...) do
     # stuff with signals
@@ -20,16 +61,57 @@ every(Hz) do
     # on periodic interval
 end
 
+
+#------------------------------------ operators ------------------------------------#
+
+# y = merge(xs...)
+# y = {xs...}
+
+# stateless maps (lift)
+# y = map(f, xs...)
+# y = f(xs...)
+# f(xs...) -> x
+
+# stateful map (fold previous)
+# y = foldp(f, xs...)
+# y = foldp(f, x)
+# y = f(x, xn-1, ..., x0)
+
+# rate limiter
+# y = limit(x, Hz)
+
+# conditional limiters
+# y = filter(f, x)
+# y = {x | f(x) is true} 
+
+# y = drop(x) # drop repeats
+# y = {x | xn != xn-1}
+
+
+#------------------------------------ useful stuff ------------------------------------#
+
+# tuple of current value and last few terms
+# x -> (x[n], x[n-1])
+
+
+#------------------------------------ other ------------------------------------#
+
+
+#= every is equivalent to
+- make a signal which gets updated periodically by taskdaemon
+    - check all times, or sort?
+- on that signal, do a thing
+=#
+
+
+
+
 # RepeatingTask - [active]
 # repeats every: Hz
 
 # ReactiveTask - [ready]
 # reacts to: ...signals...[active]
 
-
-# y = x^2
-y = map(a->a*a, x)
-# does this return a Signal? MappedSignal? ReactiveTask?
 
 
 after(sec) do
